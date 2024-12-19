d7361700c5f18c1c80650480ba225d65ce558aef
oeunkyoung <oeunkyoung@shinsegae.com>
2024-12-19 15:36:15 +0900

    test: 포인트 충전 및 사용 동시성 제어 테스트 작성

--- src/test/java/io/hhplus/tdd/point/PointConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/point/PointConcurrencyTest.java
@@ -43,6 +43,47 @@ public class PointConcurrencyTest {
         assertThat(userPoint.point()).isEqualTo(amount * threadCount);
     }
 
+    /**
+     * 여러 명이 동시에 충전할 경우
+     */
+    @Test
+    void 충전_동시성_제어_테스트2() throws InterruptedException {
+        // given
+        long[] ids = {1, 2, 3, 4, 5};
+        long amount = 100L;
+
+        for (long id : ids) {
+            pointRepository.insertOrUpdate(new UserPoint(id, 0L, System.currentTimeMillis()));
+        }
+
+        int threadCount = 20;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            int id = i % ids.length + 1; // id 당 4번 반복
+            executorService.submit(() -> {
+                try {
+                    pointService.charge(id, amount);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        for (long id : ids) {
+            Optional<UserPoint> optionalUserPoint = pointRepository.selectById(id);
+            assertThat(optionalUserPoint).isPresent();
+
+            UserPoint userPoint = optionalUserPoint.get();
+            assertThat(userPoint.point()).isEqualTo(amount * 4);
+        }
+    }
+
     @Test
     void 사용_동시성_제어_테스트() throws InterruptedException {
         // given
@@ -62,6 +103,91 @@ public class PointConcurrencyTest {
         assertThat(optionalUserPoint).isPresent();
         UserPoint userPoint = optionalUserPoint.get();
         assertThat(userPoint.point()).isEqualTo(balance - amount * threadCount);
+    }
+
+    /**
+     * 여러 명이 동시에 사용할 경우
+     */
+    @Test
+    void 사용_동시성_제어_테스트2() throws InterruptedException {
+        // given
+        long[] ids = {1, 2, 3, 4, 5};
+        long balance = 10000L;
+        long amount = 100L;
+
+        for (long id : ids) {
+            pointRepository.insertOrUpdate(new UserPoint(id, balance, System.currentTimeMillis()));
+        }
+
+        int threadCount = 20;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            int id = i % ids.length + 1; // id 당 4번 반복
+            executorService.submit(() -> {
+                try {
+                    pointService.use(id, amount);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        for (long id : ids) {
+            Optional<UserPoint> optionalUserPoint = pointRepository.selectById(id);
+            assertThat(optionalUserPoint).isPresent();
+
+            UserPoint userPoint = optionalUserPoint.get();
+            assertThat(userPoint.point()).isEqualTo(balance - amount * 4);
+        }
+    }
+
+    /**
+     * 하나의 id로 충전과 사용을 동시에 시도할 경우
+     */
+    @Test
+    void 충전_사용_동시성_제어_테스트() throws InterruptedException {
+        // given
+        long id = 0L;
+        long balance = 1000L;
+        long chargeAmount = 100L;
+        long useAmount = 30L;
+
+        pointRepository.insertOrUpdate(new UserPoint(id, balance, System.currentTimeMillis()));
+
+        // when
+        int threadCount = 20;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            int idx = i;
+            executorService.submit(() -> {
+                try {
+                    if (idx % 2 == 0) {  // 충전 사용 번갈아가면서 시도
+                        pointService.charge(id, chargeAmount);
+                    } else {
+                        pointService.use(id, useAmount);
+                    }
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        Optional<UserPoint> optionalUserPoint = pointRepository.selectById(id);
+        assertThat(optionalUserPoint).isPresent();
+
+        UserPoint userPoint = optionalUserPoint.get();
+        assertThat(userPoint.point()).isEqualTo(balance + chargeAmount * (threadCount/2) - useAmount * (threadCount/2));
     }
 
     private void executorService(int threadCount, Runnable task) throws InterruptedException {
